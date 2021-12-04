eae0b07fcd496bfa85c13e12e38600a0baea733a
Keir Kettle <keir.kettle@getbusy.com>
2021-12-04 17:39:24 +0000

    day4... urgh

--- day4/index.js
+++ day4/index.js
@@ -1,2 +1,56 @@
 import { test, puzzle } from "./input.js";
-console.log(test, puzzle);
+import _ from "lodash";
+
+const input = puzzle;
+const numbers = input[0].split(",").map((i) => parseInt(i, 10));
+const createBoards = (input) =>
+  _.chunk(
+    input.map((l) =>
+      l
+        .split(" ")
+        .filter((l) => !!l) //remove empties
+        .map((l) => ({ matched: false, v: parseInt(l, 10) }))
+    ),
+    5
+  );
+const boards = createBoards(input.slice(2).filter((line) => !!line));
+const check = (boards) => {
+  const checkInner = (row) => row.every((r) => r.matched);
+
+  for (const board of boards) {
+    for (const row of board) {
+      if (checkInner(row)) return board;
+    }
+    for (let i = 0; i < 5; i++) {
+      if (checkInner(board.map((r) => r[i]))) return board;
+    }
+  }
+  return false;
+};
+const match = (boards, val) => {
+  boards.forEach((board) => {
+    board.forEach((row) => {
+      row.forEach((item) => {
+        console.log(item.v, val);
+        if (item.v === val) {
+          item.matched = true;
+        }
+      });
+    });
+  });
+};
+
+numbers.every((n) => {
+  match(boards, n);
+  const matchingBoard = check(boards);
+  if (matchingBoard) {
+    const unmatched = matchingBoard
+      .flatMap((r) => r.map((i) => i))
+      .filter((i) => !i.matched)
+      .reduce((p, c) => p + c.v, 0);
+
+    console.log({ answer: unmatched * n });
+    return false;
+  }
+  return true;
+});
---
+++ day4/part2.js
@@ -0,0 +1,74 @@
+import { test, puzzle } from "./input.js";
+import _ from "lodash";
+
+const input = puzzle;
+const numbers = input[0].split(",").map((i) => parseInt(i, 10));
+console.log(numbers);
+const createBoards = (input) =>
+  _.chunk(
+    input.map((l) =>
+      l
+        .split(" ")
+        .filter((l) => !!l) //remove empties
+        .map((l) => ({ matched: false, v: parseInt(l, 10) }))
+    ),
+    5
+  ).map((b, i) => ({ rows: b, won: false, i }));
+let boards = createBoards(input.slice(2).filter((line) => !!line));
+const check = (board) => {
+  const checkInner = (row) => row.every((r) => r.matched);
+  for (const row of board.rows) {
+    if (checkInner(row)) {
+      board.won = true;
+      return true;
+    }
+  }
+  for (let i = 0; i < 5; i++) {
+    if (checkInner(board.rows.map((r) => r[i]))) {
+      board.won = true;
+      return true;
+    }
+  }
+  return false;
+};
+const matchBoard = (board, val) => {
+  board.rows.forEach((row) => {
+    row.forEach((item) => {
+      if (item.v === val) {
+        item.matched = true;
+      }
+    });
+  });
+  return board;
+};
+
+const winners = [];
+const getUnmatchedSum = (board) => {
+  return board.rows
+    .flatMap((r) => r.map((i) => i))
+    .filter((i) => !i.matched)
+    .reduce((p, c) => p + c.v, 0);
+};
+
+numbers.every((n) => {
+  // for every number, while some boards are not won
+  // check each board for a winner
+  // keep a list of winning boards
+  boards.filter(b => !b.won).forEach((board) => {
+    const winner = check(matchBoard(board, n));
+    if (winner) {
+      winners.push([board, n]);
+    }
+  });
+
+  return boards.some((b) => !b.won);
+});
+
+const [last, n] = _.last(winners);
+console.log(
+  winners,
+  //   sum: getUnmatchedSum(last),
+  //   last: JSON.stringify(last),
+  //   n,
+  getUnmatchedSum(last) * n,
+);
