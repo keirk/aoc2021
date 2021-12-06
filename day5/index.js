198950b3229ce8644406af2575a84eb35627e06c
Keir Kettle <keir.kettle@getbusy.com>
2021-12-06 13:43:42 +0000

    upto day6

--- day5/index.js
+++ day5/index.js
@@ -1,2 +1,59 @@
 import { test, puzzle } from "./input.js";
-console.log(test, puzzle);
+import _ from "lodash";
+
+//console.log(test, puzzle);
+const input = puzzle;
+
+const unfold = (accumulator, [startX, startY], [stopX, stopY]) => {
+  const last = _.last(accumulator);
+  if (_.isUndefined(last)) {
+    accumulator.push([startX, startY]);
+    return unfold(accumulator, [startX, startY], [stopX, stopY]);
+  }
+  let [stepX, stepY] = last;
+  if (stepX === stopX && stepY === stopY) return accumulator;
+  stepX = stepX > stopX ? stepX - 1 : stepX;
+  stepX = stepX < stopX ? stepX + 1 : stepX;
+  stepY = stepY > stopY ? stepY - 1 : stepY;
+  stepY = stepY < stopY ? stepY + 1 : stepY;
+  accumulator.push([stepX, stepY]);
+  return unfold(accumulator, [startX, startY], [stopX, stopY]);
+};
+const fill = (arr, start, end) => {
+  const steps = unfold([], start, end);
+  steps.forEach((step) => {
+    const [x, y] = step;
+    let row = arr[y];
+    if (_.isUndefined(row)) row = [];
+    if (!row[x]) {
+      row[x] = 1;
+    } else {
+      row[x] = row[x] + 1;
+    }
+    arr[y] = row;
+  });
+  return arr;
+};
+
+const answer = input
+  .map((i) => {
+    const y = i.split(" -> ");
+    const first = y[0].split(",");
+    const second = y[1].split(",");
+    return {
+      x1: parseInt(first[0], 10),
+      y1: parseInt(first[1], 10),
+      x2: parseInt(second[0], 10),
+      y2: parseInt(second[1], 10),
+    };
+  })
+  //.filter((i) => i.x1 === i.x2 || i.y1 === i.y2)
+  .reduce((arr, coord) => {
+    const start = [coord.x1, coord.y1];
+    const end = [coord.x2, coord.y2];
+    return fill(arr, start, end);
+  }, [])
+  .flatMap((i) => i)
+  .filter((i) => i >= 2).length;
+
+console.log({ answer });
--- day6/index.js
+++ day6/index.js
@@ -1,2 +1,23 @@
 import { test, puzzle } from "./input.js";
-console.log(test, puzzle);
+//console.log(test, puzzle);
+const input = test;
+
+const solve = (input, days) => {
+  const dayWiseFishCount = Array(9).fill(0);
+
+  // create buckets of fish based on age
+  input.forEach((f) => dayWiseFishCount[f]++);
+
+  for (let day = 1; day <= days; day++) {
+    // the day zeros = the fish ready to be creted
+    const fishReadyToCreate = dayWiseFishCount.shift();
+    // put the babies on day 8
+    dayWiseFishCount[8] = fishReadyToCreate;
+    // add the add adults to day 6
+    dayWiseFishCount[6] += fishReadyToCreate;
+  }
+
+  return dayWiseFishCount.reduce((c, f) => c + f, 0);
+};
+
+console.log(solve(input, 256));
